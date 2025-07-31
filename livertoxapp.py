245e8fc3c345b67b68ea44582df0d7cf50aeb9ff
Emmanuel Ogbuewu <ogbuewu2015@yahoo.com>
2025-07-31 17:27:24 -0600

    changes to livertoxapp.py

--- livertoxapp.py
+++ livertoxapp.py
@@ -34,8 +34,6 @@ from rdkit.Chem import Descriptors
 from rdkit.ML.Descriptors import MoleculeDescriptors
 from rdkit.Chem import rdMolDescriptors
 from tqdm import tqdm
-from rdkit.Chem.Draw import IPythonConsole
-IPythonConsole.drawOptions.comicMode=True
 import rdkit    
 from Mold2_pywrapper import Mold2
 import streamlit as st
@@ -142,7 +140,7 @@ def strip_isotopes(smiles):
         atom.SetIsotope(0)
     return Chem.MolToSmiles(mol, isomericSmiles=True)
 
-metal_disconnector = Standardizer().disconnect_metals
+metal_disconnector = MolVSStandardizer().disconnect_metals
 
 def disconnect_metals_from_smiles(smiles):
     mol = Chem.MolFromSmiles(smiles)
